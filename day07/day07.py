93e4fc70951f9f8d166c75a6ebe420b5e2143a99
Charl P. Botha <cpbotha@vxlabs.com>
2024-12-07 08:58:56 +0200

    add part 2

--- day07/day07.py
+++ day07/day07.py
@@ -1,5 +1,6 @@
 # started at about 8:20
 # p1 submitted 8:50
+# p2 submitted 8:56
 
 # %%
 
@@ -21,18 +22,23 @@ for line in lines:
     eqns.append((test_val, operands))
 
 
+def concat(l, r):
+    return int(str(l) + str(r))
+
+
 # p1 thoughts: minor extra effort to support additional operators
-OPS = [operator.add, operator.mul]
+# p2 thoughts: he he he -- just going to add this concat operator to the end
+OPS = [operator.add, operator.mul, concat]
 
 
 # p1 thoughts: might want to parallelize this for p2
 def check_eqn(tvo):
     test_val, operands = tvo
-    # loop through all of the permutations of add, sum
-    for ops in product([0, 1], repeat=len(operands) - 1):
+    # loop through all of the permutations of add, sum, ...
+    for ops in product(OPS, repeat=len(operands) - 1):
         ans = operands[0]
         for i, op in enumerate(ops):
-            ans = OPS[op](ans, operands[i + 1])
+            ans = op(ans, operands[i + 1])
 
         if ans == test_val:
             # there might be more solutions, but for part 1 we early-out
@@ -43,6 +49,7 @@ def check_eqn(tvo):
 
 valid_eqns = [tvo for tvo in eqns if check_eqn(tvo)]
 p1sum = sum([tvo[0] for tvo in valid_eqns])
-# 5837374519342
+# p1: 5837374519342
+# p2: 492383931650959 in 8.5 secs
 print(p1sum)
 # %%
